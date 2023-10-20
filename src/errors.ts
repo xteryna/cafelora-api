b92640f3e296757300fb67b8d3827a4cb81d2dbd
Martin Podloucký <podlouckymartin@gmail.com>
2023-10-20 09:32:38 +0200

    Přepracování na data pro jsonhost

--- src/errors.ts
+++
@@ -1,9 +0,0 @@
-import { EndpointError } from 'jsonder';
-
-export namespace Errors {
-  export const drinkNotFound = (id: string): EndpointError => ({
-    status: 404,
-    code: 'not_found',
-    detail: `Cannot find drink with id '${id}'`,
-  });
-}
